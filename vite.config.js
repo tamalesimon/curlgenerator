7036affdf729be53ce392e67e1c041b07b036316
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2024-12-02 00:24:15 +0300

    comment out base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/curlgenerator/',
+  // base:'/curlgenerator/',
   plugins: [react()],
   resolve: {
     alias: {
